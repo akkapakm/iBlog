9c21dcf4ec597a83c70fec37962fa2da9723b322
akkapakm <15688641+akkapakm@users.noreply.github.com>
2020-10-14 09:21:55 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   extends: ['@nuxtjs/eslint-config-typescript'],
   rules: {
-    semi: 'off',
-    '@typescript-eslint/semi': ['error', 'never'],
+    // semi: 'off',
+    // '@typescript-eslint/semi': ['error', 'never'],
     'arrow-parens': ['warn', 'as-needed'],
     'no-console': 'off',
     'import/order': 'off'
